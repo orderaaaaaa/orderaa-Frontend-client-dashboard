19f1865c91f7b0c6853101436164bdacff310d77
MEK <mohamedemadkamal1@gmail.com>
2025-10-13 23:11:03 +0300

    Commented the authStore file

--- src/hooks/useAuth.ts
+++
@@ -1,7 +0,0 @@
-import { useAuthStore } from '@/store/authStore';
-
-export const useAuth = () => {
-  const { user, token, isAuthenticated, login, logout, fetchProfile } =
-    useAuthStore();
-  return { user, token, isAuthenticated, login, logout, fetchProfile };
-};
