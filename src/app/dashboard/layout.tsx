48d72f4846aed7aef6feb966f0c1c0e00bd63bfd
MEK <mohamedemadkamal1@gmail.com>
2025-10-26 00:58:46 +0300

    Updated the mobile sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -184,10 +184,10 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
         {/* Sidebar */}
         <div
           className={`
-          fixed inset-y-0 left-0 z-50 transform transition-transform duration-300 ease-in-out
-          ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'}
-          lg:translate-x-0 lg:static lg:inset-0
-        `}
+    fixed inset-y-0 right-0 z-50 transform transition-transform duration-300 ease-in-out
+    ${sidebarOpen ? 'translate-x-0' : 'translate-x-full'}
+    lg:translate-x-0 lg:static lg:inset-0
+  `}
         >
           <div
             className={`
