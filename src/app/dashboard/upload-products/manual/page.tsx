b4d1522a1f8f9eca2f5849c380e4d36839159543
MEK <mohamedemadkamal1@gmail.com>
2025-10-29 19:27:07 +0300

    Merge branch 'feature/upload-products-manual' into development

--- src/app/dashboard/upload-products/manual/page.tsx
+++ src/app/dashboard/upload-products/manual/page.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+'use client';
+import { Plus } from 'lucide-react';
+import ClientInformation from './ClientInformation';
+import Order from './Order';
+import OrderDetails from './OrderDetails';
+import ShippingAndPayment from './ShippingAndPayment';
+import { useProductDropdownStore } from '@/store/productDropdownStore';
+import Products from './Products';
 
 function Manual() {
-  return <div>Manual</div>;
+  const selectedProducts = useProductDropdownStore(
+    (state) => state.selectedProducts
+  );
+  return (
+    <>
+      <div className="flex flex-col gap-[26px]">
+        <Order />
+        <OrderDetails />
+        {selectedProducts.length > 0 && <Products />}
+
+        <ClientInformation />
+        <ShippingAndPayment />
+        <div className="flex justify-end ml-6">
+          <button className="flex py-2 gap-2 text-md cursor-pointer bg-[#5D24E1] items-center text-white w-40 px-5 rounded-full font-bold">
+            {' '}
+            <Plus className="font-bold w-6 h-6" />
+            إضافة طلب
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Manual;
