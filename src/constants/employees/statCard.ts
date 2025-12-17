79328f5e21f635ca10eff6cae0cca30afca631ba
Omar-YasserVV <bakryasser744@gmail.com>
2025-12-18 01:34:14 +0200

    fix svg's for employees cards

--- src/constants/employees/statCard.ts
+++ src/constants/employees/statCard.ts
@@ -13,14 +13,14 @@ export const STAT_CARDS: StatCardConfig[] = [
     accessLevel: 'EMPLOYEE',
     borderColor: 'border-[#C9E9D2]',
     iconBgColor: 'bg-[#fbfdfb]',
-    iconPath: '/icons/employee.svg',
+    iconPath: '/Icons/employee.svg',
   },
   {
     title: 'مدير',
     accessLevel: 'MANAGER',
     borderColor: 'border-[#D2E0FB]',
     iconBgColor: 'bg-[#fbfdff]',
-    iconPath: '/icons/manager.svg',
+    iconPath: '/Icons/manager.svg',
     alt: 'Manager icon',
   },
   {
@@ -28,7 +28,7 @@ export const STAT_CARDS: StatCardConfig[] = [
     accessLevel: 'ADMIN',
     borderColor: 'border-[#2489E114]',
     iconBgColor: 'bg-[#edf6fd]',
-    iconPath: '/icons/admin.svg',
+    iconPath: '/Icons/admin.svg',
     alt: 'Admin icon',
   },
   {
@@ -36,7 +36,7 @@ export const STAT_CARDS: StatCardConfig[] = [
     accessLevel: 'SUPER_ADMIN',
     borderColor: 'border-[#5D24E114]',
     iconBgColor: 'bg-[#f2edfd]',
-    iconPath: '/icons/super-admin.svg',
+    iconPath: '/Icons/super-admin.svg',
     alt: 'Super Admin icon',
   },
   {
@@ -44,7 +44,7 @@ export const STAT_CARDS: StatCardConfig[] = [
     accessLevel: 'TOTAL',
     borderColor: 'border-[#2489E114]',
     iconBgColor: 'bg-[#edf6fd]',
-    iconPath: '/icons/total-employees.svg',
+    iconPath: '/Icons/total-employees.svg',
     alt: 'Total Employees icon',
   },
 ];
