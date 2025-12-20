1009a1aef8e9ead9ec4133e772b8a6d65e3f3529
Omar-YasserVV <bakryasser744@gmail.com>
2025-12-20 07:54:40 +0200

    add settings button to emp and 404 page

--- src/app/dashboard/employees/components/EmployeeCard.tsx
+++ src/app/dashboard/employees/components/EmployeeCard.tsx
@@ -9,6 +9,7 @@ import {
   CalendarClock,
   CalendarDays,
   Mail,
+  Edit,
 } from 'lucide-react';
 import { useUpdateEmployeeStatus } from '@/app/dashboard/employees/hooks/useEmployees';
 import {
@@ -17,6 +18,7 @@ import {
 } from '../utils/employeeMappers';
 import { AttendanceModal } from './AttendanceModal';
 import { EmployeeCardProps } from '../types/employee.types';
+import Link from 'next/link';
 
 export const EmployeeCard = memo(function EmployeeCard({
   employee,
@@ -56,9 +58,15 @@ export const EmployeeCard = memo(function EmployeeCard({
   };
 
   return (
-    <div className="bg-white rounded-2xl shadow-sm border border-gray-100 p-6 max-w-[420px]">
+    <div className="bg-white relative rounded-2xl shadow-sm border border-gray-100 p-6 max-w-[420px]">
       {/* Header */}
       <div className="flex flex-row-reverse items-center justify-end gap-5 mb-6">
+        <Link href={`employees/employee-settings/${employee.id}`}>
+          <Edit
+            size={20}
+            className="text-gray-600 absolute top-5 left-5 hover:text-gray-900 cursor-pointer"
+          />
+        </Link>
         {/* Name & Department */}
         <div className="text-right">
           <h3 className="text-2xl font-bold text-gray-900 my-4">
