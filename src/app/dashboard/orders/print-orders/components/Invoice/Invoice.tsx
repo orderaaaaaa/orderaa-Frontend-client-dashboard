8dfd3489c6d5cf2b6f12d041fa14fece2bf8daec
arsanuoshanna3-2971 <arsanuoshanna3@gmail.com>
2026-02-23 10:15:37 +0200

    for deployment

--- src/app/dashboard/orders/print-orders/components/Invoice/Invoice.tsx
+++ src/app/dashboard/orders/print-orders/components/Invoice/Invoice.tsx
@@ -132,7 +132,6 @@ export function Invoice({ data, storeInfo, language }: InvoiceProps) {
           {labels.packagingNotes}
         </span>
       </div>
-      
       <div className="grid grid-cols-2 border border-t-0 border-black">
         <div className="p-1 border-e border-black">
           {data.products.map((product, index) => (
